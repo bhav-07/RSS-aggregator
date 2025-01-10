e2ccbb25d48b6bb940ac16c134dbc7f4e8945b68
Bhavani J <jangirbhavani201@gmail.com>
2025-01-11 00:36:38 +0530

    Add DB migrations and queries

--- main.go
+++ main.go
@@ -1,24 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/bhav-07/rss-aggregator/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	DB *database.Queries
+}
+
 func main() {
-	godotenv.Load()
+	godotenv.Load(".env")
+
 	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL environment variable is not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbQueries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: dbQueries,
+	}
 
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://*", "https://*"},
+		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"*"},
 		ExposedHeaders:   []string{"Link"},
@@ -28,16 +54,17 @@ func main() {
 
 	v1Router := chi.NewRouter()
 
-	v1Router.Get("/health", handlerReadiness)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+
+	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 
 	router.Mount("/v1", v1Router)
-
-	fmt.Println("Starting server on port ", port)
-	server := &http.Server{Handler: router, Addr: ":" + port}
-
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Unable to start server")
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: router,
 	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
